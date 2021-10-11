17f0c8932e4fa0621334ef57f04827bdf09982d6
Vishal Nandanwar <nandanwar@outlook.com>
2021-10-11 09:14:09 +0530

    Sample files

--- src/First.py
+++ src/First.py
@@ -14,33 +14,34 @@ import matplotlib.pyplot as plt
 #import data set
 healtData = pd.read_csv('C:\Workspace\Extra_Activity\Machine_Learning_Activity\Health.csv')
 
+
 #Separate data features and labels
-X = healtData.iloc[:,:-1].values
+#X = healtData.iloc[:,:-1].values
 
 
-z = healtData.iloc[:,[3,4]].values
-y = healtData.iloc[:,4].values
+#z = healtData.iloc[:,[3,4]].values
+#y = healtData.iloc[:,4].values
 
-from sklearn.impute import SimpleImputer as Imputer
+#from sklearn.impute import SimpleImputer as Imputer
 
-missingValueImputer = Imputer(missing_values=np.nan,
-                              strategy="mean")
+#missingValueImputer = Imputer(missing_values=np.nan,
+#                              strategy="mean")
 
-missingValueImputer = missingValueImputer.fit(X[:,[3,4]])
+#missingValueImputer = missingValueImputer.fit(X[:,[3,4]])
 
-X[:,[3,4]] = missingValueImputer.transform(X[:,[3,4]])
+#X[:,[3,4]] = missingValueImputer.transform(X[:,[3,4]])
 
 #Delaing with categorical data
 #Encode data -- labelencoder
 #Remove mathematical weightage  -- OnehotEncoder class
 
-from sklearn.preprocessing import LabelEncoder
-X_labelencoder = LabelEncoder()
+#from sklearn.preprocessing import LabelEncoder
+#X_labelencoder = LabelEncoder()
 
-X[:,0] = X_labelencoder.fit_transform(X[:,0])
-X[:,1] = X_labelencoder.fit_transform(X[:,1])
-X[:,2] = X_labelencoder.fit_transform(X[:,2])
-X[:,5] = X_labelencoder.fit_transform(X[:,5])
+#X[:,0] = X_labelencoder.fit_transform(X[:,0])
+#X[:,1] = X_labelencoder.fit_transform(X[:,1])
+#X[:,2] = X_labelencoder.fit_transform(X[:,2])
+#X[:,5] = X_labelencoder.fit_transform(X[:,5])
 
 #X_labelencoder.classes_
 
@@ -51,14 +52,14 @@ X[:,5] = X_labelencoder.fit_transform(X[:,5])
 #Training data 80%
 #testing data 20%
 
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X,
-                                                    y,
-                                                    test_size=0.2,
-                                                    random_state=0)
+#from sklearn.model_selection import train_test_split
+#X_train, X_test, y_train, y_test = train_test_split(X,
+#                                                    y,
+#                                                    test_size=0.2,
+#                                                    random_state=0)
 
 #Step7 Feature scalling
-from sklearn.preprocessing import StandardScaler #Normal distribution
-scale = StandardScaler()
-X_train = scale.fit_transform(X_train)
-X_test = scale.fit_transform(X_test)
+#from sklearn.preprocessing import StandardScaler #Normal distribution
+#scale = StandardScaler()
+#X_train = scale.fit_transform(X_train)
+#X_test = scale.fit_transform(X_test)
